c8c2cb1528fa8314d10b20fca365c35224e949c7
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-21 11:55:08 -0600

    add goal, pseudocode to challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -1,3 +1,9 @@
+// goal:  to create a function that removes EVERY instance of "s" in a string that is given as an argument
+
+// create a function called removeLetterS
+// figure out a method to take out s (replaceAll("s", ""))
+// print result
+
 function removeLetterS(word) {
     console.log(word.replaceAll("s", ""));
 }
