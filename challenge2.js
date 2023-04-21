d66da53d5c727c32c360245651ea6d59e9923a1c
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-21 11:59:38 -0600

    add goal and pseudocode to challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -1,3 +1,10 @@
+// goal:  print every word of an array in ALL lowercase
+
+// create an array named names of strings with a mix of upper and lower case letters
+// figure out a method to change letters of a string to ALL lowercase (toLowerCase()
+// create a for statement to loop through the array and make the change to the string
+// print out each modified string
+
 var names = ["TiM", "nANcY", "MIke", "drEW"];
 
 for (var i = 0; i < names.length; i++) {
