af7e47863dd625349bd79e5b4fbc9ef7d53f5129
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-21 11:33:02 -0600

    add challenge files and challenge1 solution

---
+++ challenge1.js
@@ -0,0 +1,8 @@
+var names = ["Tim", "Beth", "Harry", "Luke", "Ian"];
+
+for (var i = 0; i < names.length; i++) {
+    //console.log(names[i].length);
+    if (names[i].length === 4) {
+        console.log(names[i]);
+    } 
+}
